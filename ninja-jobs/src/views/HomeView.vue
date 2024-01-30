df5f3ff30c430d6024b953cdbafec3ba3ff2b196
Pumpinator <alejandrodcardona5@gmail.com>
2024-01-29 22:32:01 -0600

    The Voue Router

---
+++ ninja-jobs/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus dolorem ab dolorum accusantium quod iste laboriosam, eos fugit possimus perferendis asperiores, esse natus rerum similique, eius doloribus blanditiis nihil id.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus dolorem ab dolorum accusantium quod iste laboriosam, eos fugit possimus perferendis asperiores, esse natus rerum similique, eius doloribus blanditiis nihil id.</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
